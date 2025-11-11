7ba85f9d959cbdb4c986b92c780fed37f47e9b48
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-11-11 18:16:46 +0400

    gigaseconds solution

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
 use time::PrimitiveDateTime as DateTime;
-
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    let one_billion_seconds = time::Duration::seconds(1_000_000_000);
+    start + one_billion_seconds
 }
