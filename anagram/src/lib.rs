e22140abe452e31052a4d4b3916710d627d64570
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-11-16 21:43:29 +0400

    refactored the anagram after review, using filter_map

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -6,29 +6,55 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'
     let mut word_as_chars: Vec<_> = word.chars().collect();
     word_as_chars.sort();
 
-    // create hashset for result
-    let mut result: HashSet<&str> = HashSet::new();
+    // // create hashset for result
+    // let mut result: HashSet<&str> = HashSet::new();
 
-    // find anagrams and add to the result set
-    for candidate in possible_anagrams {
-        if candidate.len() != word.len() {
-            continue;
-        }
-        let original_candidate = candidate;
-        let candidate = candidate.to_lowercase();
+    // // find anagrams and add to the result set
+    // for candidate in possible_anagrams {
+    //     if candidate.len() != word.len() {
+    //         continue;
+    //     }
+    //     let original_candidate = candidate;
+    //     let candidate = candidate.to_lowercase();
 
-        if candidate == word {
-            continue;
-        }
+    //     if candidate == word {
+    //         continue;
+    //     }
 
-        let mut candidate_as_chars: Vec<char> = candidate.chars().collect();
+    //     let mut candidate_as_chars: Vec<char> = candidate.chars().collect();
 
-        candidate_as_chars.sort();
+    //     candidate_as_chars.sort();
 
-        if candidate_as_chars == word_as_chars {
-            result.insert(original_candidate);
-        }
-    }
+    //     if candidate_as_chars == word_as_chars{
+    //         result.insert(original_candidate);
+    //     }
+    // }
 
-    result
+    // result
+
+    possible_anagrams
+        .iter()
+        .filter_map(|candidate| {
+            if candidate.len() != word.len() {
+                return None;
+            }
+            let original_candidate = candidate;
+            let candidate = candidate.to_lowercase();
+
+            if candidate == word {
+                return None;
+            }
+
+            let mut candidate_as_chars: Vec<char> = candidate.chars().collect();
+            candidate_as_chars.sort();
+
+            // if candidate_as_chars == word_as_chars {
+            //     return Some(*original_candidate);
+            // }
+            // return None;
+
+            // simplified version
+            (candidate_as_chars == word_as_chars).then_some(*original_candidate)
+        })
+        .collect()
 }
